84db178b2740ecc8825617245551a21aa132a5d5
ntddk <ntinternal@gmail.com>
2016-09-07 15:49:01 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 cd $(dirname $0)
 
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+if [ ! -f ~/.vim/colors/wombat256.vim ]; then
+    mkdir -p ~/.vim/colors/
+    git clone https://github.com/vim-scripts/wombat256.vim.git ~/wombat256.vim
+    mv ~/wombat256.vim/colors/wombat256mod.vim ~/.vim/colors/wombat256.vim 
+    rm -rf ~/wombat256.vim
 fi
 
 if [ ! -d ~/.pyenv/ ]; then
