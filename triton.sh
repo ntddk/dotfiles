10780d3285c8759d2202e9c9541e59fbbc729554
ntddk <ntinternal@gmail.com>
2017-11-13 11:48:20 +0900

    Fix permission

--- triton.sh
+++ triton.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y build-essential libcap-dev cmake libboost-all-dev libcapstone-dev gcc-multilib
 git clone https://github.com/Z3Prover/z3
 cd z3
-sudo python -E scripts/mk_make.py --python
+python -E scripts/mk_make.py --python
 cd build
 make -j $(nproc)
 sudo make install
